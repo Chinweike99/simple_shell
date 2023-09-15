d1982d7e0da5f80ae95fae5bd7e44e03550def38
Rosybliz <bchoco961@gmail.com>
2023-09-15 13:16:11 -0700

    My commit6

---
+++ utility2.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+/**
+ * auxhelp - Help information for the builtin help.
+ * Return: no return
+ */
+void auxhelp(void)
+{
+	char *help = "help: help [-dms] [pattern ...]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDisplay information about builtin commands.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Displays brief summaries of builtin commands.\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+/**
+ * helpalias - Help information for the builtin alias.
+ * Return: no return
+ */
+void helpalias(void)
+{
+	char *help = "alias: alias [-p] [name[=value]...]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDefine or display aliases.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+/**
+ * helpcd - Help information for the builtin alias.
+ * Return: no return
+ */
+void helpcd(void)
+{
+	char *help = "cd: cd [-L|[-P [-e]] [-@]] [dir]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tChange the shell working directory.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+
